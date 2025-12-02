1f501bd841b98d962cb7b2320b3fb686a000f415
Rahul <46425314+rahuldasdevops@users.noreply.github.com>
2025-12-02 21:33:02 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,18 +1,22 @@
-def call(Map params = [:]) {
-
-    node(params.agent) {
-        try {
-            stage("Initial") {
-                echo "Hello World... ${params.messages}"
-            }
-        }
-        catch (Exception ex) {
-            echo "Exception: ${ex}"
-        }
-        finally {
-            stage("Final") {
-                echo "Hello World.... ${params.final_msg}"
-            }
-        }
-    }
+def call(Map params){
+	try{
+		pipeline{
+			agent{
+				label "${params.agent}"
+			}
+			stages{
+				stage("Initial"){
+					steps{
+						echo "Hello World... ${params.messages}"
+					}
+				}
+			}
+		}
+	}
+	catch(Exception ex){
+		println "Exception: $ex"
+	}
+	finally{
+        echo "Hello World.... ${params.final_msg}"
+	}
 }
